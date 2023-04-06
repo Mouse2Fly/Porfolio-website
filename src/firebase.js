975c769450bd46a15317e7b60a5609170d8591d4
D.Navickas <112947625+DovydasNavickas0@users.noreply.github.com>
2023-04-06 22:27:05 +0300

    extra fixes

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyB3YdcDCXX5AlEXkxBvFPMElSpKi92ckXw",
-  authDomain: "react-project-66928.firebaseapp.com",
-  projectId: "react-project-66928",
-  storageBucket: "react-project-66928.appspot.com",
-  messagingSenderId: "27670058502",
-  appId: "1:27670058502:web:49d07032dcc852d2ca1587"
-};
-
-
+import { firebaseConfig } from "../firebaseKeys";
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
