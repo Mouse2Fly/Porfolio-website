ae3a317c08b70419fbcdd21c84fe8b6bb5ef23ef
D.Navickas <112947625+DovydasNavickas0@users.noreply.github.com>
2023-04-06 22:31:21 +0300

    comment added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutMe from "./components/AboutMe/AboutMe";
 import Projects from "./components/Projects/Projects";
 import Header from "./components/Header/Header";
-import Contacts from "./components/Contacts/Contacts";
+import Contacts from "./components/Contacts/Contacts"; //this will spit out error so DO NOT CHANGE IT AT ALL COSTS THIS NEED TO BE LIKE THIS
 
 import "./App.css"
 
